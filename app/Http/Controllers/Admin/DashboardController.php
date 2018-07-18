7918cf39acff609f7f97f7b1b2595dfe1d6c80bd
Andrey <msaigla@ya.ru>
2018-07-18 10:26:26 +0300

    Users count admin panel

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Article;
 use App\Category;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -16,6 +17,7 @@ class DashboardController extends Controller
             'articles'=>Article::LastArticles(5),
             'count_categories'=>Category::count(),
             'count_articles'=>Article::count(),
+            'count_users'=>User::count(),
         ]);
     }
 
---
+++ database/migrations/2018_07_10_202949_update_user.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->default('default.jpg');
+            $table->date('birthday')->default('1995.09.07');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
