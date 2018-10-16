51536510bc462dd7897a1f772e1f718a05281e05
Andrey <msaigla@ya.ru>
2018-10-16 03:45:36 +0300

    ckeditor setting

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -21,8 +21,16 @@
     </div>sas
 @endsection
 @section('individualJS')
-  <script>
-      var editor = CKEDITOR.replace( 'description' );
-      editor = CKEDITOR.replace( 'description_short' );
-  </script>
+    <script>
+        var options = {
+            filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+            filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+        };
+    </script>
+    <script>
+        CKEDITOR.replace( 'description');
+        CKEDITOR.replace( 'description_short');
+    </script>
 @endsection
